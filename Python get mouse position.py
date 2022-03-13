3980fcc26bb2b2a51cea626d41e4fec0ef2c2a98
Cristality_ <62205099+Cristality0@users.noreply.github.com>
2022-03-13 01:03:21 +0100

    Add files via upload

---
+++ Python get mouse position.py
@@ -0,0 +1,8 @@
+import pyautogui
+import time
+
+
+# for getting current position
+# run the program navigate to the correct position and after 10s the coordinates will be printed
+time.sleep(10)
+print(pyautogui.position())
